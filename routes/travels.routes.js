5c4c72e9f18af959cb808b502d446287c337ca0b
Pablo Herrero <pablohgb@gmail.com>
2023-10-07 20:43:49 +0200

    removed comments

--- routes/travels.routes.js
+++ routes/travels.routes.js
@@ -3,25 +3,10 @@ const router = express.Router()
 const Travel = require('../models/travel.model')
 const TravelCities = require('../models/travel-cities.model')
 
-// //getAll
-// router.get('/', async (req, res) => {
-//     try {
-//         const users = await User.find()
-//         res.json(users)
-//     } catch (error) {
-//         res.status(500).json({ message: error.message })
-//     }
-// })
-// //GetOne
-// router.get('/', getUser, (req, res) => {
-//     res.send(res.user.username)
-// })
-//CreateOne
 router.post('/create-travel', async (req, res) => {
     try {
         const { travelName, people, country, user, cities } = req.body;
 
-        // Create a new Travel document
         const travel = new Travel({
             travelName,
             people,
@@ -29,10 +14,8 @@ router.post('/create-travel', async (req, res) => {
             user,
         });
 
-        // Save the travel document
         const savedTravel = await travel.save();
 
-        // Create and save TravelCities documents for each city
         const travelCitiesPromises = cities.map(async (cityInfo) => {
             const { city, days } = cityInfo;
             const travelCity = new TravelCities({
@@ -51,13 +34,11 @@ router.post('/create-travel', async (req, res) => {
         res.status(500).json({ error: 'An error occurred while creating travel' });
     }
 });
-// Endpoint for updating a travel record
-app.patch('/update-travel/:id', async (req, res) => {
+router.patch('/update-travel/:id', async (req, res) => {
     try {
-        const { id } = req.params; // Extract the travel record's ObjectID from the URL
-        const updatedData = req.body; // Data to update the travel record
+        const { id } = req.params;
+        const updatedData = req.body;
 
-        // Use Mongoose to find and update the travel record
         const updatedTravel = await Travel.findByIdAndUpdate(id, updatedData, { new: true });
 
         if (!updatedTravel) {
@@ -72,13 +53,11 @@ app.patch('/update-travel/:id', async (req, res) => {
 });
 
 
-// Endpoint for updating the number of days in a city for a selected travel
-app.patch('/update-travel-city/:travelId/:cityId', async (req, res) => {
+router.patch('/update-travel-city/:travelId/:cityId', async (req, res) => {
     try {
-        const { travelId, cityId } = req.params; // Extract the travel and city ObjectIDs from the URL
-        const { days } = req.body; // Updated number of days
+        const { travelId, cityId } = req.params;
+        const { days } = req.body;
 
-        // Use Mongoose to find and update the TravelCities document
         const updatedTravelCity = await TravelCities.findOneAndUpdate(
             { travel: travelId, city: cityId },
             { days },
@@ -96,19 +75,16 @@ app.patch('/update-travel-city/:travelId/:cityId', async (req, res) => {
     }
 });
 
-// Endpoint for deleting a travel record
-app.delete('/delete-travel/:id', async (req, res) => {
+router.delete('/delete-travel/:id', async (req, res) => {
     try {
-        const { id } = req.params; // Extract the travel record's ObjectID from the URL
+        const { id } = req.params;
 
-        // Use Mongoose to find and delete the travel record
         const deletedTravel = await Travel.findByIdAndRemove(id);
 
         if (!deletedTravel) {
             return res.status(404).json({ error: 'Travel record not found' });
         }
 
-        // Also, delete associated TravelCities records (assuming you have this reference set up)
         await TravelCities.deleteMany({ travel: id });
 
         res.json({ message: 'Travel record deleted successfully' });
@@ -118,12 +94,10 @@ app.delete('/delete-travel/:id', async (req, res) => {
     }
 });
 
-// Endpoint for deleting a TravelCities document for a specific travel and city
-app.delete('/delete-travel-city/:travelId/:cityId', async (req, res) => {
+router.delete('/delete-travel-city/:travelId/:cityId', async (req, res) => {
     try {
-        const { travelId, cityId } = req.params; // Extract the travel and city ObjectIDs from the URL
+        const { travelId, cityId } = req.params;
 
-        // Use Mongoose to find and delete the TravelCities document
         const deletedTravelCity = await TravelCities.findOneAndDelete({
             travel: travelId,
             city: cityId
