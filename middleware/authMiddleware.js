aebd205d9b85b19419e2180616c6fb43eb4c7888
Pablo Herrero <pablohgb@gmail.com>
2023-10-11 10:22:34 +0200

    changed auth middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,19 +1,19 @@
 const jwt = require('jsonwebtoken');
 
 function authenticateToken(req, res, next) {
-    const authHeader = req.headers['authorization'];
+    const authHeader = req.body.token;
     const token = authHeader;
-
+    console.log("caca")
     if (token == null) {
         return res.sendStatus(401);
     }
 
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, info) => {
         if (err) {
             return res.sendStatus(403);
         }
-        req.user = user;
-        next(); // Make sure to call next to continue to the next middleware or route handler
+        req.info = info;
+        next();
     });
 }
 
