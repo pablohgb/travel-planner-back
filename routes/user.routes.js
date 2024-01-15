9eec4eb55a4ea4bd838dcb2a11e2e0e7f4c68d03
Pablo Herrero <pablohgb@gmail.com>
2024-01-15 05:36:11 +0100

    added authentication to search for a user

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,11 @@
 const Router = require("express");
 const userRouter = Router();
 const { getUsers, getOneUser, loginUser, registerUser, updateUser, deleteUser } = require("../controllers/user.controllers");
-const { getUser } = require("../middleware/authMiddleware");
+const { getUser, authenticateTokenParams } = require("../middleware/authMiddleware");
 
 userRouter
     .get('/', getUsers)
-    .get('/:id', getUser, getOneUser)
+    .get('/:token', authenticateTokenParams, getUser, getOneUser)
     .post('/login', loginUser)
     .post('/', registerUser)
     .patch('/update/:id', updateUser)
